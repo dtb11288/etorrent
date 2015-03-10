a442b0e32fa83e987610a518c491ced4623efcd5
binh <dau.thanh.binh@gmail.com>
2015-03-10 16:49:47 +0700

    add bencoding encode

--- src/lib/bencoding.erl
+++ src/lib/bencoding.erl
@@ -12,13 +12,23 @@
 %% API
 -export([encode/1, decode/1]).
 
-decode(<<$i, Rest/binary>>) ->
-    decode_integer(Rest, <<>>);
-decode(<<$l, Rest/binary>>) ->
-    decode_list(Rest, []);
-decode(<<$d, Rest/binary>>) ->
-    decode_dictionary(Rest, #{});
+% decode bencoding binary to erlang term (integer, list, binary, hashmap)
 decode(Data) ->
+    case catch dec(Data) of
+        {'EXIT', _} ->
+            {error, unparsed};
+        {Result, _} ->
+            {ok, Result}
+    end.
+
+% local decode functions
+dec(<<$i, Rest/binary>>) ->
+    decode_integer(Rest, <<>>);
+dec(<<$l, Rest/binary>>) ->
+    decode_list(Rest, []);
+dec(<<$d, Rest/binary>>) ->
+    decode_dictionary(Rest, #{});
+dec(Data) ->
     decode_string(Data, <<>>).
 
 decode_integer(<<$e, Rest/binary>>, Result) ->
@@ -36,15 +46,34 @@ decode_string(<<B, Rest/binary>>, L) ->
 decode_list(<<$e, Rest/binary>>, Result) ->
     {lists:reverse(Result), Rest};
 decode_list(Data, Result) ->
-    {R, Tail} = decode(Data),
+    {R, Tail} = dec(Data),
     decode_list(Tail, [R | Result]).
 
 decode_dictionary(<<$e, Rest/binary>>, Result) ->
     {Result, Rest};
 decode_dictionary(Data, Result) ->
-    {Key, TailValue} = decode(Data),
-    {Value, Tail} = decode(TailValue),
+    {Key, TailValue} = dec(Data),
+    {Value, Tail} = dec(TailValue),
     decode_dictionary(Tail, maps:put(Key, Value, Result)).
 
-encode(_Data) ->
-    [].
+% encode erlang term to bencoding binary
+encode(Data) ->
+    enc(Data).
+
+% local encode functions
+enc(Int) when is_integer(Int) ->
+    EncodedInt = integer_to_binary(Int),
+    <<$i, EncodedInt/binary, $e>>;
+enc(List) when is_list(List) ->
+    EncodedList = lists:foldl(fun(Item, Acc) ->
+        EncodedItem = enc(Item),
+        <<EncodedItem/binary, Acc/binary>>
+    end, <<>>, List),
+    <<$l, EncodedList, $e>>;
+enc(String) when is_binary(String) ->
+    Length = integer_to_binary(byte_size(String)),
+    <<Length/binary, $:, String/binary>>;
+%% enc(Map) when is_map(Map) ->
+%%     Map;
+enc(_) ->
+    exit(unknown_type).
